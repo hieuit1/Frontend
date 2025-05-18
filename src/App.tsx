cef6d9f9774fbdaebb51dcf45251be368e1f3479
xuanhieu12345 <hieuxuan213@gmail.com>
2025-05-18 14:43:20 +0700

    map này cần fix thêm khi merger

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import AppRouter from "./routers/AppRouter";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+
 const App: React.FC = () => {
   return (
     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ""}>
-      <AppRouter />
+        <AppRouter />
     </GoogleOAuthProvider>
   );
 };
